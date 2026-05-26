65517075e2e634649eadd29dd4594e12e8b84378
Daniele <daniele.buondonno@studenti.unimi.it>
2026-05-26 16:07:00 +0200

    initial implementation of AgentData

---
+++ Belief/AgentData.js
@@ -0,0 +1,39 @@
+class AgentData{
+    
+    id = "";
+    name = "";
+    pos = {x: -1, y : -1};
+    parcels = new Map();
+    baggedParcels = new Map();
+
+    constructor(){
+        this.id = "";
+        this.name = "";
+        /**
+         * @type {{x: number, y: number}}
+         */
+        this.pos = {x: -1, y: -1};
+        /**
+         * @type { Map string,  < id:string, carriedBy?: string, x: number, y: number, reward: number > }
+         */
+        this.parcels = new Map();
+        /**
+         * @type { Map string, < id:string, x: number, y: number, reward: number > }
+         */
+        this.baggedParcels = new Map();
+    }
+
+    get_carried_score = () => {
+        let total = 0;
+        for (bgdParcelKey of this.baggedParcels.keys()) {
+            if (this.parcels.has(bgdParcelKey)) {
+                baggedParcel = this.baggedParcels.get(bgdParcelKey)
+                total += baggedParcel.reward
+            }
+        }
+        console.log(total);
+        return total;
+    }
+}
+
+export {AgentData}
