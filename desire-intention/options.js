91bce73077c95d24c32f5ca06ae35595d4373f70
SashaPetkovic <sasha.petkovic8@gmail.com>
2026-06-05 01:06:57 +0200

    Update import paths after folder and file renames

--- desire-intention/options.js
+++ desire-intention/options.js
@@ -1,6 +1,6 @@
 import { socket } from "../connection.js";
-import { agentData, gameData } from "../Belief/Belief.js";
-import { distance } from "../Belief/GameData.js";
+import { agentData, gameData } from "../belief/Belief.js";
+import { distance } from "../belief/GameData.js";
 
 const GO_DELIVER_THRESHOLD = 5;     // threshold to make the agent go deliver parcels if he has X * averageParcelReward parcels in its bag
 
